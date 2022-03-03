5dfb819923c3207ca55c61d4b09cc137c4b0a98a
Zziroo <tlslrs1994@naver.com>
2022-03-03 13:22:57 +0900

    Feat : 9498(시험 성적)

---
+++ 9498_시험 성적/9498_시험 성적.cpp
@@ -0,0 +1,38 @@
+﻿// 9498_시험 성적.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int grade;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> grade;
+
+	if (grade >= 90)
+	{
+		cout << "A";
+	}
+	else if (80 <= grade && grade < 90)
+	{
+		cout << "B";
+	}
+	else if (70 <= grade && grade < 80)
+	{
+		cout << "C";
+	}
+	else if (60 <= grade && grade < 70)
+	{
+		cout << "D";
+	}
+	else
+	{
+		cout << "F";
+	}
+}
