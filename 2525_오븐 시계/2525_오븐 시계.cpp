409843f978352c497f630e47189407ee9e1e076a
Zziroo <tlslrs1994@naver.com>
2022-03-03 14:03:40 +0900

    Feat : 2525(오븐 시계)

---
+++ 2525_오븐 시계/2525_오븐 시계.cpp
@@ -0,0 +1,34 @@
+﻿// 2525_오븐 시계.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int A, B;
+int C;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> A >> B;
+	cin >> C;
+
+	B += C;
+
+	while (B >= 60)
+	{
+		++A;
+		B -= 60;
+
+		if (A >= 24)
+		{
+			A = 0;
+		}
+	}
+
+	cout << A << " " << B;
+}
