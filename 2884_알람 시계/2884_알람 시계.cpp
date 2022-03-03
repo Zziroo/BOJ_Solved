f6bb29720ded37cc206dd0385c503df115e98d3b
Zziroo <tlslrs1994@naver.com>
2022-03-03 13:47:42 +0900

    Feat : 2884(알람 시계)

---
+++ 2884_알람 시계/2884_알람 시계.cpp
@@ -0,0 +1,34 @@
+﻿// 2884_알람 시계.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int H, M;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> H >> M;
+
+	if (M < 45)
+	{
+		--H;
+		M += 15;
+
+		if (H < 0)
+		{
+			H = 23;
+		}
+	}
+	else
+	{
+		M -= 45;
+	}
+
+	cout << H << " " << M;
+}
