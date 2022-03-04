657d30d703a68ed41f0ea14446a5efd2559b5541
Zziroo <tlslrs1994@naver.com>
2022-03-05 02:17:46 +0900

    Feat : 8393(합)

---
+++ 8393_합/8393_합.cpp
@@ -0,0 +1,25 @@
+﻿// 8393_합.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	int sum = 0;
+	for (int i = N; i > 0; --i)
+	{
+		sum += i;
+	}
+
+	cout << sum;
+}
