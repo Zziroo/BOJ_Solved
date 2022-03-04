67b0d08f547ccc35039be683e70911f87a1fca85
Zziroo <tlslrs1994@naver.com>
2022-03-05 02:13:47 +0900

    Feat : 10950(A+B - 3)

---
+++ 10950_A+B - 3/10950_A+B - 3.cpp
@@ -0,0 +1,27 @@
+﻿// 10950_A+B - 3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int T;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> T;
+
+	for (int i = 0; i < T; ++i)
+	{
+		int num1 = 0;
+		int num2 = 0;
+
+		cin >> num1 >> num2;
+
+		cout << num1 + num2 << "\n";
+	}
+}
