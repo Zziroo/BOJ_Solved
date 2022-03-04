22f6c6a38b13ea40977f902a3686d6672cde7dd9
Zziroo <tlslrs1994@naver.com>
2022-03-04 11:37:59 +0900

    Feat : 2739(구구단)

---
+++ 2739_구구단/2739_구구단.cpp
@@ -0,0 +1,22 @@
+﻿// 2739_구구단.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		cout << N << " * " << i + 1 << " = " << N * (i + 1) << "\n";
+	}
+}
