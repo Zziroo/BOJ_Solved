ee81383c760c434ec855668300658c00c54ccd69
Zziroo <tlslrs1994@naver.com>
2022-03-07 19:56:43 +0900

    Feat : 2741(N 찍기)

---
+++ 2741_N 찍기/2741_N 찍기.cpp
@@ -0,0 +1,22 @@
+﻿// 2741_N 찍기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		cout << i + 1 << "\n";
+	}
+}
