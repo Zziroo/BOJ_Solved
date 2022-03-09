b8e0634a171453c3a672970dfcf4b54dc82011bd
Zziroo <tlslrs1994@naver.com>
2022-03-10 03:41:22 +0900

    Feat : 11021(A+B - 7)

---
+++ 11021_A+B - 7/11021_A+B - 7.cpp
@@ -0,0 +1,27 @@
+﻿// 11021_A+B - 7.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int T;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> T;
+
+	for (int i = 0; i < T; ++i)
+	{
+		int A = 0;
+		int B = 0;
+
+		cin >> A >> B;
+
+		cout << "Case #" << i + 1 << ": " << A + B << "\n";
+	}
+}
