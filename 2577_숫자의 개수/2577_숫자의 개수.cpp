70ddc138b007898cd2e458a139a3bef1344cd708
Zziroo <tlslrs1994@naver.com>
2022-03-22 15:53:14 +0900

    Feat : 2577(숫자의 개수)

---
+++ 2577_숫자의 개수/2577_숫자의 개수.cpp
@@ -0,0 +1,50 @@
+﻿// 2577_숫자의 개수.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int A;
+int B;
+int C;
+int arr[10];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> A;
+	cin >> B;
+	cin >> C;
+
+	int multiplication = A * B * C;
+
+	while (true)
+	{
+		int temp = multiplication % 10;
+
+		for (int i = 0; i < 10; ++i)
+		{
+			if (i == temp)
+			{
+				++arr[i];
+				break;
+			}
+		}
+
+		if (multiplication < 10)
+		{
+			break;
+		}
+
+		multiplication /= 10;
+	}
+
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << arr[i] << "\n";
+	}
+}
