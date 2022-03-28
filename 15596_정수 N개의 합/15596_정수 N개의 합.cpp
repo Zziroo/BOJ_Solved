f7e18d62698751f72c1538cc2ab7bcfb12041db9
Zziroo <tlslrs1994@naver.com>
2022-03-28 13:01:42 +0900

    Feat : 15596(정수 N개의 합)

---
+++ 15596_정수 N개의 합/15596_정수 N개의 합.cpp
@@ -0,0 +1,24 @@
+﻿// 15596_정수 N개의 합.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long sum(vector<int>& a)
+{
+	long long ans = 0;
+
+	for (size_t i = 0; i < a.size(); ++i)
+	{
+		ans += a[i];
+	}
+
+	return ans;
+}
+
+int main()
+{
+	std::cout << "정수 N개의 합\n";
+}
