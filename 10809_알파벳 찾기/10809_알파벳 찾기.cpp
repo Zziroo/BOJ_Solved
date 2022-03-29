4b0cb96f8c405028288cec4c058b35f969785c18
Zziroo <tlslrs1994@naver.com>
2022-03-29 11:53:14 +0900

    Feazt : 10809(알파벳 찾기)

---
+++ 10809_알파벳 찾기/10809_알파벳 찾기.cpp
@@ -0,0 +1,44 @@
+﻿// 10809_알파벳 찾기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+char cha[102];
+
+int alphabet[26];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	fill_n(alphabet, 26, -1);
+
+	cin >> cha;
+
+	int index = 0;
+
+	while ('\0' != cha[index])
+	{
+		int alphaIndex = cha[index] - 'a';
+
+		if (-1 != alphabet[alphaIndex])
+		{
+			++index;
+
+			continue;
+		}
+
+		alphabet[alphaIndex] = index;
+
+		++index;
+	}
+
+	for (int i = 0; i < 26; ++i)
+	{
+		cout << alphabet[i] << " ";
+	}
+}
