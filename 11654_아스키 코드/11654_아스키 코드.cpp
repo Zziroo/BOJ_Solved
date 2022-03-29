bd9ecfe5483adfcd90ef01aa55f95c58683fbd20
Zziroo <tlslrs1994@naver.com>
2022-03-29 11:53:54 +0900

    Feat : 11654(아스키 코드)

---
+++ 11654_아스키 코드/11654_아스키 코드.cpp
@@ -0,0 +1,22 @@
+﻿// 11654_아스키 코드.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+char cha;
+int num;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> cha;
+
+	num = static_cast<int>(cha);
+
+	cout << num;
+}
