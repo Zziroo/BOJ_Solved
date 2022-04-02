d5e8afdfc38fd38d9c02797d99dbdfadb10ced9c
Zziroo <tlslrs1994@naver.com>
2022-04-02 12:47:36 +0900

    Feat : 1152(단어의 개수)

---
+++ 1152_단어의 개수/1152_단어의 개수.cpp
@@ -0,0 +1,43 @@
+﻿// 1152_단어의 개수.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+char cha[1'000'002];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin.getline(cha, sizeof(cha), '\n');
+
+	bool isWord = false;
+
+	int count = 0;
+	int index = 0;
+
+	while ('\0' != cha[index])
+	{
+		if (' ' != cha[index])
+		{
+			if (false == isWord)
+			{
+				isWord = true;
+
+				++count;
+			}
+		}
+		else
+		{
+			isWord = false;
+		}
+
+		++index;
+	}
+
+	cout << count;
+}
