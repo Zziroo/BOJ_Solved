082362934b3eec53c4b7d8e3ddcf2f47640ecfb9
Zziroo <tlslrs1994@naver.com>
2022-04-04 13:09:22 +0900

    Feat : 2292(벌집)

---
+++ 2292_벌집/2292_벌집.cpp
@@ -0,0 +1,37 @@
+﻿// 2292_벌집.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	int count = 0;
+	int compareNum = 1;
+	int order = 0;
+
+	while (true)
+	{
+		++count;
+
+		compareNum += 6 * order;
+
+		if (N <= compareNum)
+		{
+			break;
+		}
+
+		++order;
+	}
+
+	cout << count;
+}
