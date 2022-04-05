31888e686fa5ceaccf3665f7dd091bda817f6282
Zziroo <tlslrs1994@naver.com>
2022-04-05 14:23:10 +0900

    Fix : Modify solving method with binary search

--- 10816_숫자 카드 2/10816_숫자 카드 2.cpp
+++ 10816_숫자 카드 2/10816_숫자 카드 2.cpp
@@ -1,13 +1,14 @@
 ﻿// 10816_숫자 카드 2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 int N;
 int M;
-int arr[20'000'002];
+int arr[500'002];
 
 int main()
 {
@@ -17,21 +18,23 @@ int main()
 
 	cin >> N;
 
-	int temp = 0;
-
 	for (int i = 0; i < N; ++i)
 	{
-		cin >> temp;
-
-		++arr[10'000'000 + temp];
+		cin >> arr[i];
 	}
 
+	sort(arr, arr + N);
+
 	cin >> M;
+
+	int temp = 0;
 
 	for (int i = 0; i < M; ++i)
 	{
 		cin >> temp;
 
-		cout << arr[10'000'000 + temp] << " ";
+		cout << upper_bound(arr, arr + N, temp) - lower_bound(arr, arr + N, temp) << " ";
 	}
+
+
 }
