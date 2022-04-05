4b2d798c42d278b90dadfc07ad84a348c575fe58
Zziroo <tlslrs1994@naver.com>
2022-04-05 11:52:43 +0900

    Feat : 2869(달팽이는 올라가고 싶다)

---
+++ 2869_달팽이는 올라가고 싶다/2869_달팽이는 올라가고 싶다.cpp
@@ -0,0 +1,36 @@
+﻿// 2869_달팽이는 올라가고 싶다.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int A;
+int B;
+int V;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> A >> B >> V;
+
+	int elapsedDate = 0;
+
+	++elapsedDate;
+
+	V -= A;
+
+	int climbDistance = A - B;
+
+	elapsedDate += V / climbDistance;
+
+	if (0 != (V % climbDistance))
+	{
+		++elapsedDate;
+	}
+
+	cout << elapsedDate;
+}
