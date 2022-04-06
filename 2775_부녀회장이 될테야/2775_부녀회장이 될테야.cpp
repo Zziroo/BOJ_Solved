876ab9a9ef74c97c3414631748ff0ce392af3d28
Zziroo <tlslrs1994@naver.com>
2022-04-06 12:54:19 +0900

    Fix : Fix sum number method

--- 2775_부녀회장이 될테야/2775_부녀회장이 될테야.cpp
+++ 2775_부녀회장이 될테야/2775_부녀회장이 될테야.cpp
@@ -8,27 +8,31 @@ using namespace std;
 int T;
 int k;
 int n;
-
-int SumNum(int k, int n)
-{
-	if (1 == n)
-	{
-		return 1;
-	}
-
-	if (0 == k)
-	{
-		return n;
-	}
-
-	return SumNum(k, n - 1) + SumNum(k - 1, n);
-}
+int arr[15][15];
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
+
+	for (int i = 0; i < 15; ++i)
+	{
+		arr[0][i] = i + 1;
+	}
+
+	for (int i = 0; i < 15; ++i)
+	{
+		arr[i][0] = 1;
+	}
+
+	for (int i = 1; i < 15; ++i)
+	{
+		for (int j = 1; j < 15; ++j)
+		{
+			arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+		}
+	}
 
 	cin >> T;
 
@@ -37,8 +41,6 @@ int main()
 		cin >> k;
 		cin >> n;
 
-		int count = SumNum(k, n);
-
-		cout << count << "\n";
+		cout << arr[k][n - 1] << "\n";
 	}
 }
