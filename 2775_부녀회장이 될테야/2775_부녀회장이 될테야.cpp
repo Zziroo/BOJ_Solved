d31464fdbc7e4f5ceecad1afa575029dd95a26be
Zziroo <tlslrs1994@naver.com>
2022-04-06 12:27:06 +0900

    Feat : 2775(부녀회장이 될테야)

---
+++ 2775_부녀회장이 될테야/2775_부녀회장이 될테야.cpp
@@ -0,0 +1,44 @@
+﻿// 2775_부녀회장이 될테야.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int T;
+int k;
+int n;
+
+int SumNum(int k, int n)
+{
+	if (1 == n)
+	{
+		return 1;
+	}
+
+	if (0 == k)
+	{
+		return n;
+	}
+
+	return SumNum(k, n - 1) + SumNum(k - 1, n);
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> T;
+
+	for (int i = 0; i < T; ++i)
+	{
+		cin >> k;
+		cin >> n;
+
+		int count = SumNum(k, n);
+
+		cout << count << "\n";
+	}
+}
