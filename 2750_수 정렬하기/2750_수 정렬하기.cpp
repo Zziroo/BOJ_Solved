c54664335bb04afedee5389031de7f2827efdf99
Zziroo <tlslrs1994@naver.com>
2022-04-06 14:15:18 +0900

    Feat : 2750(수 정렬하기)

---
+++ 2750_수 정렬하기/2750_수 정렬하기.cpp
@@ -0,0 +1,31 @@
+﻿// 2750_수 정렬하기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1'002];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		cout << arr[i] << "\n";
+	}
+}
