083e4d656a49a8ce104bea8e0b8e34a4b1932dc5
Zziroo <tlslrs1994@naver.com>
2022-04-07 18:58:00 +0900

    Feat : 10814(나이순 정렬)

---
+++ 10814_나이순 정렬/10814_나이순 정렬.cpp
@@ -0,0 +1,44 @@
+﻿// 10814_나이순 정렬.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<pair<int, string>> members;
+
+bool Compare(pair<int, string> lhs, pair<int, string> rhs)
+{
+	if (lhs.first < rhs.first)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	members.resize(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> members[i].first >> members[i].second;
+	}
+
+	stable_sort(members.begin(), members.end(), Compare);
+
+	for (int i = 0; i < N; ++i)
+	{
+		cout << members[i].first << " " << members[i].second << "\n";
+	}
+}
