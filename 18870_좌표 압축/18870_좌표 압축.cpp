dfd4c02f0a521a802d4d9bdf846d2e0075990287
Zziroo <tlslrs1994@naver.com>
2022-04-07 21:04:41 +0900

    Feat : 18870(좌표 압축)

---
+++ 18870_좌표 압축/18870_좌표 압축.cpp
@@ -0,0 +1,41 @@
+﻿// 18870_좌표 압축.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> arr;
+vector<int> sortArr;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	arr.resize(N);
+	sortArr.resize(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> arr[i];
+		sortArr[i] = arr[i];
+	}
+
+	sort(sortArr.begin(), sortArr.end());
+
+	sortArr.erase(unique(sortArr.begin(), sortArr.end()), sortArr.end());
+
+	for (int i = 0; i < N; ++i)
+	{
+		auto it = lower_bound(sortArr.begin(), sortArr.end(), arr[i]);
+
+		cout << it - sortArr.begin() << " ";
+	}
+}
