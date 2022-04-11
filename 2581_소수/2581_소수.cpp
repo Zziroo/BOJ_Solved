671bebab1434ab49c3ec69d91b9665388db202ec
Zziroo <tlslrs1994@naver.com>
2022-04-11 12:44:51 +0900

    Feat : 2581(소수)

---
+++ 2581_소수/2581_소수.cpp
@@ -0,0 +1,54 @@
+﻿// 2581_소수.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int M;
+int N;
+vector<int> primes;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> M >> N;
+
+	for (int i = M; i <= N; ++i)
+	{
+		int count = 0;
+
+		for (int j = 1; j <= i; ++j)
+		{
+			if (0 == i % j)
+			{
+				++count;
+			}
+		}
+
+		if (2 == count)
+		{
+			primes.push_back(i);
+		}
+	}
+
+	if (0 == primes.size())
+	{
+		cout << "-1";
+
+		return 0;
+	}
+
+	int sumNum = 0;
+
+	for (size_t i = 0; i < primes.size(); ++i)
+	{
+		sumNum += primes[i];
+	}
+
+	cout << sumNum << "\n" << primes[0];
+}
