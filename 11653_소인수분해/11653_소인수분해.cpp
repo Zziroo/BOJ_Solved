48fa833eb4fe5c546708763f7092f904fe885059
Zziroo <tlslrs1994@naver.com>
2022-04-12 14:56:34 +0900

    Feat : 11653(소인수분해)

---
+++ 11653_소인수분해/11653_소인수분해.cpp
@@ -0,0 +1,37 @@
+﻿// 11653_소인수분해.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	cin >> N;
+
+	if (1 == N)
+	{
+		return 0;
+	}
+
+	for (int i = 2; i <= N; ++i)
+	{
+		while (0 == N % i)
+		{
+			cout << i << "\n";
+
+			N /= i;
+		}
+
+		if (1 == N)
+		{
+			break;
+		}
+	}
+}
