00563914c33cdd04e2baa808445ef88cf83355c1
Zziroo <tlslrs1994@naver.com>
2022-04-24 20:39:51 +0900

    Feat : 10872(팩토리얼)

---
+++ 10872_팩토리얼/10872_팩토리얼.cpp
@@ -0,0 +1,34 @@
+﻿// 10872_팩토리얼.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int Factorial(int num)
+{
+	if (0 == num)
+	{
+		return 1;
+	}
+
+	if (1 == num)
+	{
+		return 1;
+	}
+
+	return num * Factorial(num - 1);
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	cout << Factorial(N);
+}
