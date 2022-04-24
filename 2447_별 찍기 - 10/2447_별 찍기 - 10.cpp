1b4dc97533dc53b926183b57ac5046c41f6899c0
Zziroo <tlslrs1994@naver.com>
2022-04-24 21:34:49 +0900

    Feat : 2447(별 찍기 - 10)

---
+++ 2447_별 찍기 - 10/2447_별 찍기 - 10.cpp
@@ -0,0 +1,46 @@
+﻿// 2447_별 찍기 - 10.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void PrintStar(int row, int column, int num)
+{
+	if (1 == (row / num) % 3 && 1 == (column / num) % 3)
+	{
+		cout << " ";
+	}
+	else
+	{
+		if (0 == num / 3)
+		{
+			cout << "*";
+		}
+		else
+		{
+			PrintStar(row, column, num / 3);
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+
+	for (int row = 0; row < N; ++row)
+	{
+		for (int column = 0; column < N; ++column)
+		{
+			PrintStar(row, column, N);
+		}
+
+		cout << "\n";
+	}
+}
