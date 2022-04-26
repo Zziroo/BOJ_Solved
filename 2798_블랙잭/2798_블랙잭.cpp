45b88eab82f54ebf42f3b491b4aa93f3d531918d
Zziroo <tlslrs1994@naver.com>
2022-04-26 13:37:54 +0900

    Feat : 2798(블랙잭)

---
+++ 2798_블랙잭/2798_블랙잭.cpp
@@ -0,0 +1,45 @@
+﻿// 2798_블랙잭.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int N;
+int M;
+int arr[101];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + N);
+
+	int sum = 0;
+
+	for (int i = 0; i < N - 2; ++i)
+	{
+		for (int j = i + 1; j < N - 1; ++j)
+		{
+			for (int k = j + 1; k < N; ++k)
+			{
+				if (arr[i] + arr[j] + arr[k] >= sum && arr[i] + arr[j] + arr[k] <= M)
+				{
+					sum = arr[i] + arr[j] + arr[k];
+				}
+			}
+		}
+	}
+
+	cout << sum;
+}
