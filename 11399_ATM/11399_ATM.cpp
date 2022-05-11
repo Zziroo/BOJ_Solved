2ef6d41a479d0892253f3d7ad4a9e5fa76a404d7
Zziroo <tlslrs1994@naver.com>
2022-05-11 17:12:54 +0900

    Feat : 11399(ATM)

---
+++ 11399_ATM/11399_ATM.cpp
@@ -0,0 +1,35 @@
+﻿// 11399_ATM.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int N;
+int P[1'000];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> P[i];
+	}
+
+	sort(P, P + N);
+
+	int totalTime = 0;
+
+	for (int i = 0; i < N; ++i)
+	{
+		totalTime += (N - i) * P[i];
+	}
+
+	cout << totalTime;
+}
